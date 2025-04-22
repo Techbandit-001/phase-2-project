196fa1c0ac18ce35ce5755c9417f33ec02c0ab95
raphael kihiro <kihirongigi@gmail.com>
2025-04-22 22:58:15 +0300

    listening to events

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { RecipeForm } from './RecipeForm'
-//import { RecipeForm } from './RecipeForm'
-//import {RecipeForm} from "./RecipeForm"
+
 
 export function App() {
   return (
--- src/RecipeForm.jsx
+++ src/RecipeForm.jsx
@@ -1,10 +1,38 @@
+import { useState } from 'react'
+
 export function RecipeForm() {
-  
+    const initialData= {
+        image:"",
+        title:"",
+        description:'',
+        preparationTime:"",
+        category:""    }
+    const [form, setForm] = useState(initialData);
+// Dynamically adding an event listener that handles the submission of the form 
+
+  const handleSubmit = (e) => {
+    // line 8 is used to ensure that the page doesn't reload on submission
+    e.preventDefault();
+    //   
+    console.log("form submitted");
+    console.log(form);
+    // used to reset the form to its intial status 
+     setForm(initialData);
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+      setForm((prev) => ({
+        // to return the form to its previous state which is empty values 
+      ...prev,
+      [name]: value,
+    }));
+  };
 
     return (
       <div>
-      <form className="border p-2 rounded-xl w-full bg-amber-100" 
-      action="">
+      <form className="border p-2 rounded-xl w-full bg-amber-100 space-y-3" 
+      action="" onSubmit={handleSubmit}>
+
         <div>
         <label htmlFor="title">Title</label>
         <input 
@@ -12,25 +40,46 @@ export function RecipeForm() {
         className='border p-2 bg-white w-full rounded-lg'
          type="text"
          placeholder="Ingredients"
+         name="title"
+         value={form.title}
+         onChange={handleChange}
          required />
          </div>
+
          <div>
             <label htmlFor="description">Description</label>
         <textarea 
         className='border p-2 bg-white w-full rounded-lg'
-         name=""
+         name="description"
          placeholder="Description"
-          id="description">
+          id="description"
+          value={form.description}
+          onChange={handleChange}
+          >
           </textarea>
           </div>
+           <div>
+            <label htmlFor="preparation time">Prepartion Time</label>
+            <input
+            placeholder='Preparation Time' 
+            id='preparationTime'
+            name='preparationTime'
+            type="text" 
+            className="border p-2 bg-white w-full rounded-lg"
+            value={form.preparationTime}
+            onChange={handleChange}
+            />
+          </div> 
+
          <div>
             <label htmlFor="category">Category</label>
             <select
-             name="Category" 
+             name="category" 
              id="category"
              placeholder='Category'
              className="border p-2 bg-white w-full rounded-lg"
-
+             value={form.category}
+             onChange={handleChange}
              >
                 <option value =''>Select a category</option>
                 <option value ='breakfast'>Breakfast</option>
@@ -39,6 +88,20 @@ export function RecipeForm() {
                 <option value ='dinner'>Dinner</option>
              </select>
         </div> 
+
+        <div>
+        <label htmlFor="image">Image</label>
+        <input 
+        id="image"
+        className='border p-2 bg-white w-full rounded-lg'
+         type="text"
+         placeholder="image"
+         name='image'
+         value={form.image}
+         onChange={handleChange}
+         />
+         </div>
+
         <div className="mt-4">
             <button className="border rounded-lg w-full p-2 bg-amber-500 hover:bg-gray-600">Add recipes</button>
         </div>
