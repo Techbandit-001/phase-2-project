b167c3bbdea85da438767846351af9721d6683c7
Robert Haji <roberthadji66@gmail.com>
2025-04-23 13:47:24 +0300

    initial commits

---
+++ Search.jsx
@@ -0,0 +1,9 @@
+// Search
+import { Search } from "lucide-react"; //icon for search installed via npm install lucide-react
+export function SearchBar() {
+  return (
+    <div>
+      <label htmlFor="search">Search Recipes:</label>
+    </div>
+  );
+}
