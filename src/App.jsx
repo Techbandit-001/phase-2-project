790d828b47e06c7d39980b13e96c9bd182a81c5e
michelle <michellemuchoki8@gmail.com>
2025-04-23 16:19:59 +0300

    receip-list done

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,31 @@
-import { useState } from 'react'
-import { RecipeForm } from './RecipeForm'
-raphael-recipeform
+import { useEffect, useState } from "react";
+import { RecipeForm } from "./RecipeForm";
+import { RecipeList } from "./RecipeList";
 
-David-Nav-Footer
-import Footer from './Footer'
-import NavBar from './NavBar'
+import Footer from "./Footer";
+import NavBar from "./NavBar";
 //import { RecipeForm } from './RecipeForm'
 //import {RecipeForm} from "./RecipeForm"
-import { ReceiptList } from './RecipeList'
-main
-main
 
 export function App() {
   return (
     <>
-    <header> 
-    <NavBar/>
+      <header>
+        <NavBar />
       </header>
-    <main className='min-h-screen flex p-8 gap-3'>
-      <div className='w-[35%]'>
-        <strong>+ Add recipes</strong>
+      <main className="min-h-screen flex p-8 gap-3">
+        <div className="w-[35%]">
+          <strong>+ Add recipes</strong>
           <RecipeForm />
-          <ReceiptList/>
-      </div>
-      <div className='flex-1'> Recipes </div>
-    </main>
+        </div>
+        <div className="flex-1">
+          Recipes
+          <RecipeList />
+        </div>
+      </main>
       <footer>
-        <Footer/>
-    </footer>
-      
+        <Footer />
+      </footer>
     </>
-  )
+  );
 }
-
-
