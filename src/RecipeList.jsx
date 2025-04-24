001c5af0560ea074226f74538e531d08edb8c2aa
michelle <michellemuchoki8@gmail.com>
2025-04-24 16:20:35 +0300

    changes

--- src/RecipeList.jsx
+++ src/RecipeList.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { RecipeCard } from "./RecipeCard";
 
 export function RecipeList({recipes}) {
+  
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
