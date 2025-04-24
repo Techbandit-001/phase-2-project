c16d03d699f7c6ae4b7f8b4ec5b75113c15366b1
David Kiptum <davy@DESKTOP-7A71LJO.>
2025-04-24 16:06:25 +0300

    adds pages and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import{App}from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutUs from "./pages/About.jsx";
+import ContactUs from "./pages/Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/About",
+    element: <AboutUs />,
+  },
+
+  // {
+  //   path: "/Favorites",
+  //   element: <Favorites />
+  // },
+
+  {
+    path: "/Contact",
+    element: <ContactUs />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </StrictMode>
+);
