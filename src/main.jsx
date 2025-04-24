4b0ac01f7f0f4e6886eb0e9173dca4289c4487b1
Techbandit-001 <davidkiptum80@gmail.com>
2025-04-24 18:44:01 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
+
+import { App } from "./App.jsx";
 import AboutUs from "./pages/About.jsx";
 import ContactUs from "./pages/Contact.jsx";
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
- import { Toaster } from 'react-hot-toast'
-import './index.css'
-import{App}from './App.jsx'
-
 
 const router = createBrowserRouter([
   {
